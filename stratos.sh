94e944c0418613d0bbf8e92ab24f524d60a39ac1
gary-on-github <101473124+gary-on-github@users.noreply.github.com>
2022-03-26 13:19:25 +0000

    Update stratos.sh

--- stratos.sh
+++ stratos.sh
@@ -28,7 +28,13 @@ options=(
 )
 
 function node_install  { 
-sudo apt upgrade -y < "/dev/null"
+
+export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_PRIORITY=critical
+sudo -E apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
+sudo -E apt-get -qy autoclean
+
+
 sudo apt install mc jq curl build-essential git wget -y
 sudo rm -rf /usr/local/go
 curl https://dl.google.com/go/go1.17.linux-amd64.tar.gz | sudo tar -C /usr/local -zxvf -
