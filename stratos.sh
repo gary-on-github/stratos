38d37991275b31818980962f38d5a76dc913f3f3
gary-on-github <101473124+gary-on-github@users.noreply.github.com>
2022-03-26 23:02:56 +0000

    Update stratos.sh

--- stratos.sh
+++ stratos.sh
@@ -19,7 +19,8 @@ function show_feedback {
 
 prompt='Select:'
 options=(
-    "Stratos Testnet Installation"
+    "Stratos Testnet Installation IPV4"
+    "Stratos Testnet Installation IPV6"
     "Check Wallet"
     "Node Status"
     "Journalctl"
@@ -29,6 +30,89 @@ options=(
 )
 
 function node_install  { 
+
+sudo rm -rf /usr/local/go
+curl https://dl.google.com/go/go1.17.linux-amd64.tar.gz | sudo tar -C /usr/local -zxvf -
+
+cat <<'EOF' >>$HOME/.profile
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export GO111MODULE=on
+export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+EOF
+
+source $HOME/.profile
+sleep 1
+read -p "Please enter your node ID: " node_id
+echo 'Your node ID is : ' $node_id
+echo 'export node_id='$node_id >> $HOME/.bash_profile
+read -p "Please enter your wallet name : " wallet_name
+echo 'Your wallet name is : ' $wallet_name
+echo 'export wallet_name='$wallet_name >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+cd "$HOME" || exit
+wget https://github.com/stratosnet/stratos-chain/releases/download/v0.7.0/stchaincli
+wget https://github.com/stratosnet/stratos-chain/releases/download/v0.7.0/stchaind
+sudo chmod +x stchaincli
+sudo chmod +x stchaind
+
+echo 'export PATH="$HOME:$PATH"' >> ~/.profile
+source ~/.profile
+
+./stchaind init $node_id
+
+wget https://raw.githubusercontent.com/stratosnet/stratos-chain-testnet/main/tropos-3/genesis.json
+wget https://raw.githubusercontent.com/stratosnet/stratos-chain-testnet/main/config.toml
+
+mv config.toml $HOME/.stchaind/config/
+mv genesis.json $HOME/.stchaind/config/
+
+sed -i "s/"node"/$node_id/g" "$HOME/.stchaind/config/config.toml"
+
+
+cd $HOME
+echo '{ "height": "0", "round": "0", "step": 0 }' > .stchaind/data/priv_validator_state.json
+
+peers="a97214289b659dca9db98963959bde117851b485@52.194.30.100:26656,92c6a339999d0ab972698f6c28b69dd134c3a834@75.119.146.247:26656,90c18307c235c456ebdc127b98de503b30994599@54.189.208.239:26656"
+
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" ~/.stchaind/config/config.toml
+
+routable_ip="$(curl ifconfig.me)"
+sed -i.bak -e "s/^external_address *=.*/external_address = \"tcp:\/\/\[$routable_ip]:26656\"/" ~/.stchaind/config/config.toml
+
+./stchaincli keys add $wallet_name --hd-path "m/44'/606'/0'/0/0" --keyring-backend=test  
+echo "Please make a note of your mnemonic and press enter once to continue"
+
+sudo tee <<EOF >/dev/null /etc/systemd/system/stratos.service
+[Unit]
+Description=Stratos Chain Node
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$HOME/stchaind start --home=$HOME/.stchaind
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=8192
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+Storage=persistent
+EOF
+
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable stratos.service
+sudo systemctl start stratos.service
+echo "Stratos Testnet Instalation has Finished & Started"
+
+}
+
+function node_install6  { 
 
 sudo rm -rf /usr/local/go
 curl https://dl.google.com/go/go1.17.linux-amd64.tar.gz | sudo tar -C /usr/local -zxvf -
@@ -110,6 +194,9 @@ sudo systemctl start stratos.service
 echo "Stratos Testnet Instalation has Finished & Started"
 
 }
+
+
+
 function check_wallet   { 
 cd "$HOME" || exit
 source $HOME/.bash_profile
@@ -149,12 +236,13 @@ function show_menu {
   select option in "${options[@]}"; do
     case $REPLY in 
        1 ) node_install             ; break ;;
-       2 ) check_wallet             ; break ;;
-       3 ) node_status              ; break ;;
-       4 ) check_journalctl         ; break ;;
-       5 ) stop_stratos_service     ; break ;;
-       6 ) start_stratos_service    ; break ;;
-       7 ) quit                     ; break ;;
+       2 ) node_install6            ; break ;;
+       3 ) check_wallet             ; break ;;
+       4 ) node_status              ; break ;;
+       5 ) check_journalctl         ; break ;;
+       6 ) stop_stratos_service     ; break ;;
+       7 ) start_stratos_service    ; break ;;
+       8 ) quit                     ; break ;;
        * ) not_option               ; break ;;
     esac
   done
